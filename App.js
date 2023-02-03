ef4466c44c89a26f5ef54d9a0635266cbc3bda5c
Shaikh RAFIK <107038358+ShaikhRafik786@users.noreply.github.com>
2023-02-04 02:39:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,62 @@
+// import logo from "./logo.svg//
+import "./App.css";
+// import { Home } from "./Components/Sachin/Home";
+// import TodoList from "./Components/TODOLIST/TodoList";
+// import Srk from "./Components/Srk";
+// import Fetch from "./Components/Fetch";
+// import {ApiData} from "./Components/ITERVIEW/ApiData";
+// import { Counter } from "./Components/Counter";
+// import Login from "./Components/Formfill/Login";
+import TodoList from "./Components/TODOLIST/TodoList";
+// import { ApiData } from "./Components/ITERVIEW/ApiData"
+// import TodoItem from "./Components/TODOLIST/TodoItem";
+// import { Home } from "./Components/Redux/Home";
+// import Navigation from "./Components/Redux/Navigation";
+// import Dashboard from "./Components/Redux/Dashboard";
+// import CustomCarousel from "./Components/Redux/CustomCarousel";
+// import Courses from "./Components/Redux/Courses";
+// import Landing from "./Components/E-Card Project/Landing";
+// import TodoList from "./Components/TodoList";
+// import TodoList from "./Components/Todo-CRUD/TodoList";
+// import { Counter } from './Components/Counter';
+// import CounterClass from "./Components/CounterClass";
+// import {ToDoList} from "./Components/ITERVIEW/TODoList";
+// import  { Login } from "./Components/LOGIN FORM/Login";
+// import {Register} from "./Components/LOGIN FORM/Register";
+// import Dashboard from "./Components/LOGIN FORM/Dashboard";
+function App() {
+  return (
+    <div className="App">
+      {/* <TodoItem/> */}
+      {/* <Counter/> */}
+      {/* <TodoList/> */}
+      {/* <CounterClass/> */}
+      {/* <TodoList /> */}
+      {/* <Landing/> */}
+      {/* <ToDoList/> */}
+      {/* /* <BrowserRouter>
+        <Router>
+          <Routes>
+            <Route path="./" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Dashboard" element={<Dashboard />} />
+          </Routes>
+        </Router>
+      </BrowserRouter> */}
+      {/* <Home/> */}
+      {/* <Dashboard />
+      <Navigation/>
+      <CustomCarousel/>
+      <Courses/> */}
+      {/* <Counter/>
+      // {/* <ApiData/> */}
+      {/* <Fetch/> */}
+      {/* <Srk/> */}
+      {/* <Home/>/ */}
+     {/* <Login/> */}
+     <TodoList/>
+    </div>
+  );
+}
+
+export default App;
